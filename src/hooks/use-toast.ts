b03dd8d294fa7df4fd58b1fbf5a41f7829ff8923
tahaamin2013 <tahaamindob2013@gmail.com>
2025-10-15 11:43:01 -0700

    d

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -3,7 +3,7 @@
 // Inspired by react-hot-toast library
 import * as React from 'react'
 
-import type { ToastActionElement, ToastProps } from '@/components/ui/toast'
+import type { ToastActionElement, ToastProps } from '../components/ui/toast'
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
@@ -155,7 +155,8 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
-      onOpenChange: (open) => {
+      // @ts-ignore
+      onOpenChange: (open: string) => {
         if (!open) dismiss()
       },
     },
